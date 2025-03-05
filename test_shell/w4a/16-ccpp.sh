d4959ce19a7673f76d013582a50192e3fefc3146
宋刚 <1006516550@qq.com>
2025-03-05 17:54:00 +0800

    ccpp

--- test_shell/w4a/16-ccpp.sh
+++ test_shell/w4a/16-ccpp.sh
@@ -1,6 +1,6 @@
 gnome-terminal --window -e 'bash -c "roscore; exec bash"' \
 --tab -e 'bash -c "sleep 3; roslaunch upros_bringup bringup_w4a.launch; exec bash"' \
 --tab -e 'bash -c "sleep 3; roslaunch upros_navigation navigation.launch; exec bash"' \
---tab -e 'bash -c "sleep 3; roslaunch clean_robot area_path.launch; exec bash"' \
+--tab -e 'bash -c "sleep 3; roslaunch upros_navigation area_path.launch; exec bash"' \
 --tab -e 'bash -c "sleep 3; roslaunch upros_navigation view_nav.launch; exec bash"' \
---tab -e 'bash -c "sleep 13; roslaunch clean_robot next_goal.launch; exec bash"' \
+--tab -e 'bash -c "sleep 13; roslaunch upros_navigation next_goal.launch; exec bash"' \
